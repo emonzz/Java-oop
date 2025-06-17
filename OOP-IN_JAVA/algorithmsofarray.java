63a2a9b8eccd77d84046b0f244b325be8016ec7f
emonzz <149598470+emonzz@users.noreply.github.com>
2025-06-17 14:07:58 +0100

    Add files via upload

---
+++ OOP-IN_JAVA/algorithmsofarray.java
@@ -0,0 +1,22 @@
+  package basics;
+    import java.util
+  no usages
+  public class ArrayExample {
+  public static void main(String{}args) {
+
+    
+
+
+  int[] myArray = {1,3,5,7,9};
+  int[] anotherArray = new int[10];
+  String[] gender = {"Female", "Male"};
+
+    myArray[1] = 13;
+    myArray[2] = 13;
+    myArray[3] = 14;
+    anotherArray[5] = 17;
+ for (int i = 0; < myArray.length; i++) {
+        system.out.println("item at: "+i+ "is" +myArray[i]);
+       }
+    }
+ }
---
+++ OOP-IN_JAVA/src/algorithm of array.java
@@ -0,0 +1,6 @@
+package basics;
+
+no usages
+public class ArrayExample {
+    
+}
